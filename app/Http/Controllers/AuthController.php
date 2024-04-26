10777b942b307cc1d53d60b7569e5c00e5527248
imronarth <imron@gmx.com>
2024-04-26 14:56:57 +0700

    add base54

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use Melihovv\Base64ImageDecoder\Base64ImageDecoder;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -20,8 +23,34 @@ class AuthController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['errors' => $validator->messages()], 400);
+            return response()->json(['messages' => $validator->messages()], 400);
         }
         $user = User::where('email', $request->email)->exists();
+
+        if ($user) {
+            return response()->json(['messages' => 'email already taken'], 409);
+        }
+        try {
+            $profilePicture = null;
+            $ktp = null;
+            if ($request->profile_picture) {
+                $this->uploadBase64Image($request->profile_picture);
+            }
+            if ($request->ktp) {
+                $this->uploadBase64Image($request->ktp);
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+    private function uploadBase64Image($imgUpload)
+    {
+        $decoder = new Base64ImageDecoder($imgUpload, $allowedFormats = ['jpeg', 'png', 'gif']);
+        $decodedContent = $decoder->getDecodedContent();
+        $format = $decoder->getFormat();
+        $image = Str::random(10) . '.' . $format;
+        Storage::disk('public')->put($image, $decodedContent);
+        return $image;
     }
 }
